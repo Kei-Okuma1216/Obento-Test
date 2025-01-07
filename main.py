4c354ee8c75f2b77fc08cbca886d9a3cc76d07c5
Kei Okuma <k.okuma@ten-system.com>
2025-01-07 13:21:50 +0900

    localhostで単純な長文文字列返した

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+"""
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+"""
+@app.get("/")
+def read_root():
+    # ここで単純に localhost にアクセスした場合
+    return {"signature": "ここにBase64Encode文字列がズラズラと書いてある。"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
