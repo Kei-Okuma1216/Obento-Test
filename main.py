2856ce14e203bc1e2563b0c151e19e04ba5dba54
Kei Okuma <k.okuma@ten-system.com>
2025-01-11 14:01:24 +0900

    Cookieha

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from cryptography.hazmat.primitives.asymmetric import padding
-from fastapi import FastAPI, Form, Query
+from fastapi import FastAPI, Form, Query , Response
 from fastapi.responses import FileResponse, HTMLResponse
 from datetime import datetime
 from typing import Union # 型ヒント用モジュール
@@ -21,16 +20,31 @@ async def read_root(request: Request):
 
 # 登録完了画面
 @app.post("/register", response_class=HTMLResponse)
-async def register(request: Request, username: str = Form(...), password: str = Form(...)):
+async def register(
+    #request: Request, username: str = Form(...), password: str = Form(...)):
+    request: Request, response: Response, username: str = Form(...), password: str = Form(...)):
     try:
-        today_date = datetime.today()
-        token = create_jwt(username, password, today_date)
+        # Cookieを設定 
+        #id = 1
+        #formatted_id = str(id).zfill(3)
+        #response.set_cookie(key="id", value=formatted_id)
+        response.set_cookie(key="id", value="001")
+        print(f"Cookie id: 001")
+        
+        today_date = datetime.today().strftime('%Y-%m-%d %H:%M:%S') 
+        response.set_cookie(key="regist_date", value=today_date)
+        print(f"regist_date: {today_date}")
+        
+        # トークンをCookieに設定 
+        token = create_jwt(username, password, datetime.today())
+        response.set_cookie(key="token", value=token)
         print(f"Generated JWT: {token}")
 
-        # テンプレートにtokenを渡す 
+        # tokenを表示 
         return templates.TemplateResponse(
             "regist_complete.html", {"request": request, "token": token})
     except Exception as e: 
+        print(f"Error: {str(e)}")
         return templates.TemplateResponse(
             "error.html", {"request": request, "error": str(e)})
 
@@ -51,26 +65,30 @@ uvicorn main:app --host 0.0.0.0 --port 8000 --ssl-keyfile=./my-local.key --ssl-c
 # /abcを加えた場合    
 @app.get("/abc", response_class=HTMLResponse)
 async def read_abc(request: Request, token: str = Query(...)):
-    payload = verify_jwt(token)
-    if payload:
-        print(f"Token is valid. Payload: {payload}")
-        
-        # 現在の日付
-        cur_date = datetime.now()
-        # 日付を取り出す 
-        token_date = datetime.fromisoformat(payload["date"]) 
-        print(f"Retrieved Date: {token_date}")
-        
-        # abc.html
-        return templates.TemplateResponse("abc.html",
-            {"request": request, "token": token,
-             "retrieved_date": token_date, "current_date": cur_date})
-    else:
-        # token_error.html
-        print("Invalid or expired token.") 
-        return templates.TemplateResponse("token_error.html",
-            {"request": request, "token": token},status_code=400
-            )
+    try:
+        payload = verify_jwt(token)
+        if payload:
+            print(f"Token is valid. Payload: {payload}")
+            
+            # 現在の日付
+            cur_date = datetime.now()
+            # 日付を取り出す 
+            token_date = datetime.fromisoformat(payload["date"]) 
+            print(f"Retrieved Date: {token_date}")
+            
+            # abc.html
+            return templates.TemplateResponse("abc.html",
+                {"request": request, "token": token,
+                "retrieved_date": token_date, "current_date": cur_date})
+        else:
+            # token_error.html
+            print("Invalid or expired token.") 
+            return templates.TemplateResponse("token_error.html",
+                {"request": request, "token": token},status_code=400)
+    except Exception as e:
+        print(f"Error: {str(e)}") 
+        return templates.TemplateResponse( 
+            "error.html", {"request": request, "error": str(e)})
 
 
 # ブラウザが要求するfaviconのエラーを防ぐ
