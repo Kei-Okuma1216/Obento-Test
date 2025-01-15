958273ae112a29e2852e3a3b55e15decc34f331b
Kei Okuma <k.okuma@ten-system.com>
2025-01-15 15:14:29 +0900

    チュートリアルでヘッダからCookie取得まで

--- FastAPI_tutorial/main.py
+++ FastAPI_tutorial/main.py
@@ -1,86 +1,70 @@
-from enum import Enum
-from typing import Union
+# パスパラメータ
+# https://fastapi.tiangolo.com/ja/tutorial/path-params-numeric-validations/
+from typing import List, Union
 
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Path, Query
+from pydantic import BaseModel, Field
 
+class Image(BaseModel):
+    url: str
+    name: str
+    
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(
+        gt=0, description="The price must be greater than zero")
+    tax: Union[float, None] = None
+    tags: List[str] = []
+    image: Union[Image, None] = None
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
 
-class Planet(str, Enum):
-    earth = "earth"
-    sun = "sun"
-    moon = "moon"
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
 
-from fastapi import FastAPI, Response, Cookie
-
-# uvicorn main:app --reload
 app = FastAPI()
 
-# http://127.0.0.1:8000/users/1001/items/foo?q=bar&short=false
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    print(f"short: {short}")
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-@app.get("/set-cookie") 
-def set_cookie(response: Response): 
-    response.set_cookie(key="my_cookie", value="cookie_value") 
-    return {"message": "Cookie set"}
-
-# http://127.0.0.1:8000/items/foo?short=true
-# http://127.0.0.1:8000/items/foo?q=bar&short=false
-@app.get("/items/{item_id}")
-async def read_item(
-    item_id: str,
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(gt=0),
     q: Union[str, None] = None,
-    short: bool = False):
-    
-    item = {"item_id": item_id}
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
     if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-# ページング
-# http://127.0.0.1:8000/items/?skip=0&limit=10
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"}]
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-
-
-# http://127.0.0.1:8000/files/home/johndoe/myfile.txt
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
-
-# http://127.0.0.1:8000/planets/earth
-@app.get("/planets/{model_name}")
-async def get_model(model_name: Planet = Planet.earth):
-    
-    if model_name is Planet.sun: 
-        return {"model_name": model_name, "message": "Sun!"} 
-    elif model_name is Planet.moon: 
-        return {"model_name": model_name, "message": "Moon!"} 
-    elif model_name is Planet.earth: 
-        return {"model_name": model_name, "message": "Earth!"} 
-    else: return {"model_name": model_name, "message": "No!"}
-
-
+        results.update({"q": q})
+    return results
+'''
+@app.get("/items/{item_id}")
+async def read_items(
+    q: str, item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+    size: float = Query(gt=0, lt=10.5)):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if size:
+        results.update({"size": size})
+    return results
+'''
+# ブラウザではGETになりエラーになる
+# POST確認は別ターミナルで実行する
+# curl -X POST "http://127.0.0.1:8000/items/" -H "Content-Type: application/json" -d "{\"name\": \"Sample Item\", \"description\": \"This is a sample item.\", \"price\": 10.0, \"tax\": 0.5}"
 
 
---
+++ FastAPI_tutorial/old1_main.py
@@ -0,0 +1,92 @@
+from enum import Enum
+from typing import Union
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+class Planet(str, Enum):
+    earth = "earth"
+    sun = "sun"
+    moon = "moon"
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    
+
+
+# uvicorn main:app --reload
+app = FastAPI()
+
+# http://127.0.0.1:8000/users/1001/items/foo?q=bar&short=false
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    print(f"short: {short}")
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+@app.get("/set-cookie") 
+def set_cookie(response: Response): 
+    response.set_cookie(key="my_cookie", value="cookie_value") 
+    return {"message": "Cookie set"}
+
+# http://127.0.0.1:8000/items/foo?short=true
+# http://127.0.0.1:8000/items/foo?q=bar&short=false
+@app.get("/items/{item_id}")
+async def read_item(
+    item_id: str,
+    q: Union[str, None] = None,
+    short: bool = False):
+    
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# ページング
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+
+# http://127.0.0.1:8000/files/home/johndoe/myfile.txt
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+# http://127.0.0.1:8000/planets/earth
+@app.get("/planets/{model_name}")
+async def get_model(model_name: Planet = Planet.earth):
+    
+    if model_name is Planet.sun: 
+        return {"model_name": model_name, "message": "Sun!"} 
+    elif model_name is Planet.moon: 
+        return {"model_name": model_name, "message": "Moon!"} 
+    elif model_name is Planet.earth: 
+        return {"model_name": model_name, "message": "Earth!"} 
+    else: return {"model_name": model_name, "message": "No!"}
+
+
+
+
