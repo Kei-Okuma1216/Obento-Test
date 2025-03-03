b9948473c81f2f798ff7c86be535cf8a010a2737
Kei Okuma <k.okuma@ten-system.com>
2025-03-03 11:44:51 +0900

    SQL例外クラスを実装した

--- v_0.1.3/app/utils/exception.py
+++ v_0.1.3/app/utils/exception.py
@@ -1,7 +1,9 @@
 
 from fastapi import HTTPException
 from starlette import status
-
+'''
+encoded_message = urllib.parse.quote(f"login_get() Error:  {e.detail}")
+'''     
 class CustomException(HTTPException):
     def __init__(self, status_code: int, detail: str):
         print(f"🚨 CustomException 発生！ status_code={status_code}, message={detail}")  # 追加
@@ -9,17 +11,33 @@ class CustomException(HTTPException):
 
 
 # Token期限切れ例外クラス
-class TokenExpiredException(HTTPException):
+class TokenExpiredException(CustomException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
 
 # 認証不許可クラス
-class NotAuthorizedException(HTTPException):
+class NotAuthorizedException(CustomException):
     def __init__(self):
         super().__init__(status_code=status.HTTP_403_FORBIDDEN, detail="Not Authorized")
 
 # Cookie取得失敗クラス
-class CookieException(HTTPException):
-    def __init__(self):
-        super().__init__(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, detail="Cookieが取得できませんでした")
+class CookieException(CustomException):
+    def __init__(self, detail: str = "Cookieが取得できませんでした"):
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
 
+# SQLエラー例外クラス
+# 例
+# raise SQLException(sql_statement=query, detail=str(e))
+class SQLException(CustomException):
+    def __init__(self, sql_statement: str, detail: str = "An error occurred with the SQL operation"):
+        #super().__init__(status_code=status.#HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
+        full_detail = f"{detail}: SQL statement = {sql_statement}"
+        print(f"🚨 SQLException 発生！ SQL文: {sql_statement}")  # SQL文をログに出力
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=full_detail)
+
+# データベース接続エラー例外クラス
+# 例 raise DatabaseConnectionException(detail=str(e))
+class DatabaseConnectionException(CustomException):
+    def __init__(self, detail: str = "Failed to connect to the database"):
+        print("データベース接続の確立に失敗しました。")
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
