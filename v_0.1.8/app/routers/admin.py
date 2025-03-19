d19f533c576066af97568d02456b15a004fd9d63
Kei Okuma <k.okuma@ten-system.com>
2025-03-19 10:46:35 +0900

    ログ閲覧直した

--- v_0.1.8/app/routers/admin.py
+++ v_0.1.8/app/routers/admin.py
@@ -1,30 +1,40 @@
 # admin.py
 import bcrypt
 import os
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from database.sqlite_database import update_user, get_all_users
 from utils.exception import CustomException
-from utils.utils import check_permission, log_decorator
+from utils.utils import log_decorator
 from venv import logger
 
 templates = Jinja2Templates(directory="templates")
 
 admin_router = APIRouter()
 
+@log_decorator
+def check_admin_permission(request: Request):
+    # 管理者の権限チェック
+    permission = request.cookies.get("permission")
+    #print(f"permission: {permission}")
+    if permission != "99":
+        raise CustomException(
+            status.HTTP_401_UNAUTHORIZED,
+            "check_admin_permission()",
+            f"Not Authorized permission={permission}")
+
 # 管理者画面
 # 注意：エンドポイントにprefix:adminはつけない
 @admin_router.get("/me", response_class=HTMLResponse, tags=["admin"])
 @log_decorator
-async def admin_view(request: Request):    
+def admin_view(request: Request):    
 
-    if await check_permission(request, [99]):
-        return templates.TemplateResponse(
-            "admin.html", {"request": request})
-    else:
-        return templates.TemplateResponse(
-            "Unauthorized.html", {"request": request})
+    # 権限チェック
+    check_admin_permission(request)
+
+    return templates.TemplateResponse(
+        "admin.html", {"request": request})
 
 @log_decorator
 @admin_router.get("/me/update_existing_passwords", response_class=HTMLResponse, tags=["admin"])
@@ -44,46 +54,6 @@ async def update_existing_passwords():
             await update_user(
                 user.username, "password", new_hashed_password)  # DB更新
             logger.info(f"ユーザー {user.username} のパスワードをハッシュ化しました")
-
-@admin_router.get("/me/check_device", response_class=HTMLResponse, tags=["admin"])
-async def check_device(request: Request):
-    '''使い方
-    #await init_database()
-    value = await check_device(request)
-    # JSON をテキストとして整形
-    text_value = json.dumps(value, ensure_ascii=False, indent=4)
-    logger.info(text_value)
-    '''
-    user_agent = request.headers.get("user-agent", "").lower()
-
-    # 簡単なスマホ判定
-    is_mobile = any(keyword in user_agent for keyword in ["iphone", "android", "mobile"])
-
-    return {"user_agent": user_agent, "is_mobile": is_mobile}
-
-
-
-@admin_router.get("/logs", response_class=HTMLResponse)
-def list_logs():
-    # logsディレクトリ内のファイル一覧を取得
-    log_dir = "logs"
-    if not os.path.exists(log_dir):
-        return "<h1>ログディレクトリが存在しません</h1>"
-    log_files = sorted(os.listdir(log_dir), reverse=True)
-    # 各ログファイルへのリンクを作成
-    links = [f"<li><a href='/admin/logs/{file}'>{file}</a></li>" for file in log_files]
-    return f"<h1>ログ一覧</h1><ul>{''.join(links)}</ul>"
-
-@admin_router.get("/logs/{filename}", response_class=HTMLResponse)
-def view_log(filename: str):
-    log_path = os.path.join("logs", filename)
-    if os.path.exists(log_path):
-        with open(log_path, "r", encoding="utf-8") as f:
-            # 改行を<br>に置換してHTML上で見やすく表示
-            content = f.read().replace('\n', '<br>')
-        return f"<h1>{filename}</h1><pre>{content}</pre>"
-    else:
-        return "ログファイルが存在しません。"
 
 
 '''
@@ -108,3 +78,27 @@ async def custom_exception_handler(
 async def test_exception():
     raise CustomException(400, "test_exception()", "これはテストエラーです")
 '''
+# logsディレクトリ内のファイル一覧を取得
+@admin_router.get("/logs", response_class=HTMLResponse)
+def list_logs():
+
+    log_dir = "logs"
+    if not os.path.exists(log_dir):
+        return "<h1>ログディレクトリが存在しません</h1>"
+    log_files = sorted(os.listdir(log_dir), reverse=True)
+    # 各ログファイルへのリンクを作成
+    links = [f"<li><a href='/admin/logs/{file}'>{file}</a></li>" for file in log_files]
+    
+    return f"<h1>ログ一覧</h1><ul>{''.join(links)}</ul>"
+
+# ログファイル表示
+@admin_router.get("/logs/{filename}", response_class=HTMLResponse)
+def view_log(filename: str):
+    log_path = os.path.join("logs", filename)
+    if os.path.exists(log_path):
+        with open(log_path, "r", encoding="utf-8") as f:
+            # 改行を<br>に置換してHTML上で見やすく表示
+            content = f.read().replace('\n', '<br>')
+        return f"<h1>{filename}</h1><pre>{content}</pre>"
+    else:
+        return "ログファイルが存在しません。"
