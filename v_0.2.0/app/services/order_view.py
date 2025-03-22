b822d13057aac86a7c7429777904aa484b164345
Kei Okuma <k.okuma@ten-system.com>
2025-03-22 15:26:53 +0900

    get_order_json()は0が取得できない

--- v_0.2.0/app/services/order_view.py
+++ v_0.2.0/app/services/order_view.py
@@ -5,12 +5,11 @@ from fastapi import HTTPException, APIRouter, Query, Request, Response, status
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 
-from database.sqlite_database import DatabaseConnectionException, get_connection, select_shop_order, select_user
-from main import SQLException, get_all_cookies
+from database.sqlite_database import get_connection, select_shop_order, select_user
+from main import get_all_cookies
 templates = Jinja2Templates(directory="templates")
 from database.sqlite_database import CustomException
 from utils.utils import log_decorator 
-import aiosqlite
 
 view_router = APIRouter(
     prefix="/orders",
@@ -53,74 +52,7 @@ async def order_table_view(request: Request, response: Response, orders, redirec
             status.HTTP_400_BAD_REQUEST,
             f"/order_table_view()",
             f"Error: {str(e)}")
-        #print(f"/order_table_view Error: {str(e)}")
-        #return JSONResponse({"message": "エラーが発生しました"},
-        #                    e.status_code)
-'''
-# 注文情報JSON取得
-@log_decorator
-async def get_order_json(request: Request, days_ago: str = Query(None)):
-    # 入力例 /order_json?days_ago=-5
-    # @app.get("/order_json",response_class=HTMLResponse) 
-    try:
-        cookies = get_all_cookies(request)
-        if not cookies:
-            #return HTMLResponse("<html><p>ユーザー情報が取得できませんでした。</p></html>")
-            return JSONResponse({"error": "ユーザー情報が取得できませんでした。"}, status_code=400)
 
-        # 注文追加
-        user = await select_user(cookies['sub'])
-
-        if user is None:
-            logger.debug(f"user:{user} 取得に失敗しました")
-            return JSONResponse({"error": "ユーザー情報が取得できませんでした。"}, status_code=400)
-
-        # 履歴取得処理
-        if days_ago is not None:
-            logger.debug(f"days_ago: {int(days_ago)}")
-
-        #print(f"days_ago: {days_ago}")
-        # 履歴取得
-        if days_ago is None or days_ago == '':
-            logger.debug("全履歴を取得する") 
-            orders = await select_shop_order(user.shop_name)
-
-        elif days_ago.isdigit() or (days_ago.startswith('-') and days_ago[1:].isdigit()):
-            logger.info(f"{days_ago} 日前までの履歴を取得する")
-
-            orders = await select_shop_order(user.shop_name, days_ago)
-        else:
-            logger.debug("days_ago の値が無効です")
-            return JSONResponse({"error": "days_ago の値が無効です"}, status_code=400)
-
-        if not orders:
-            logger.info("No orders found or error occurred.")
-            return JSONResponse({"message": "注文が見つかりません。"}, status_code=404)
-
-        # 日時で逆順
-        orders.sort(key=lambda x: x.created_at, reverse=True)
-
-        #print("ここまできた 3")
-        for order in orders:
-            logger.debug(order.model_dump_json())
-
-        #print("ここまできた 4")
- 
-        orders_dict = [order.model_dump() for order in orders]
-        orders_json = json.dumps(orders_dict, default=str)  # ← datetime を文字列に変換
-
-        #return JSONResponse(content=json.loads(orders_json))  # JSON をパースしてレスポンス
-        return JSONResponse(content=json.loads(orders_json), media_type="application/json; charset=utf-8")
-
-    except DatabaseConnectionException as e:
-        raise
-    except SQLException as e:
-        raise
-    except Exception as e:
-        orders = []
-        logger.warning(f"/order_json Error: {str(e)}")
-        return JSONResponse({"error": f"エラーが発生しました: {str(e)}"}, status_code=500)
-'''
 @log_decorator
 async def get_order_json(request: Request, days_ago: str = Query(None)):
     try:
