0954bc89cebadb406f2a04b1ced745a69ee00f43
Kei Okuma <k.okuma@ten-system.com>
2025-03-28 10:25:55 +0900

    v_0.0.1-0.1.2まで追跡解除と削除した

--- v_0.1.2/app/tests/test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM cd /d "C:\Obento-Test\v_0.1.2\app\tests"
-REM cd /d %~dp0  REM testsフォルダに移動
-REM C:\Obento-Test\v_0.1.2\app\tests
-REM pytest test_login3.py
-REM pytest test_login3.py -s | tee pytest_results.log
-pytest test_login3.py -s --log-file=pytest_log.txt
-pause
