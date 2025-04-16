240b1e7082edce62d12a6212d075f24c6e38e99f
Kei Okuma <k.okuma@ten-system.com>
2025-04-16 11:44:14 +0900

    fix: setting.pyを追加した

--- v_0.2.1/app/database/local_postgresql_database.py
+++ v_0.2.1/app/database/local_postgresql_database.py
@@ -2,25 +2,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-import psycopg2
+# postgreSQL用設定
+from .settings import settings   # settings は .env から環境変数をロード
+DATABASE_URL = settings.database_url
 
-# データベースとのコネクションを確立します。
-from sqlalchemy.ext.asyncio import create_async_engine
-
-'''
-DATABASE_URL = "postgresql+asyncpg://postgres:root@localhost/LunchOrder"
-engine = create_async_engine(DATABASE_URL, echo=True)
-'''
-DATABASE_URL = "sqlite+aiosqlite:///database/example.db"
-
-db_name_str = "example.db"
-
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # 現在のファイルの絶対パスを取得
-DB_PATH = os.path.join(BASE_DIR, db_name_str)  # 
 
 # 非同期エンジン作成
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False) # echo=Trueでログを出力する
 
 # セッションファクトリ
 AsyncSessionLocal = sessionmaker(
@@ -33,9 +21,6 @@ AsyncSessionLocal = sessionmaker(
 Base = declarative_base()
 
 # データベースセッションを取得する関数
-'''async def get_db():
-    async with AsyncSessionLocal() as session:
-        yield session'''
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -46,11 +31,9 @@ async def get_db():
         finally:
             await session.close()
 
-
-
-# ここのIPアドレスを変更したら、呼び出しスクリプトも同様に変更する。
-endpoint = "https://192.168.3.14:8000"
-
+# 定数
+ENDPOINT = settings.endpoint # "https://192.168.3.14:8000"
+endpoint = ENDPOINT
 default_shop_name = "shop01"
 default_company_id = 1
 default_compamy_name = "テンシステム"
---
+++ v_0.2.1/app/settings.py
@@ -0,0 +1,26 @@
+# database/settings.py
+# manual of setting management
+# https://docs.pydantic.dev/2.11/concepts/pydantic_settings/
+
+# from typing import Any
+
+# from pydantic import (
+#     BaseModel,
+#     Field,
+#     AliasChoices,
+#     ImportString,
+#     PostgresDsn,
+# )
+# from pydantic import ConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+    database_url: str
+    endpoint: str
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+# 設定インスタンスを生成（どこでもインポートして利用できます）
+settings = Settings()
+
+print(Settings().model_dump()) # 本番環境ではコメントアウトする
+
